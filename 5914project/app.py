e3917bf48aa3876be86513f02c059601c32fb84f
lin2772 <46533075+lin2772@users.noreply.github.com>
2020-04-14 19:00:49 -0400

    add search function to "/p" page

--- 5914project/app.py
+++ 5914project/app.py
@@ -5,6 +5,7 @@ from html import unescape
 import json
 from ibm_watson import AssistantV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator, BasicAuthenticator
+from search_movie_by_title import requestIBMD
 
 app = Flask(__name__)
 
@@ -55,11 +56,14 @@ def index():
 def movie():
     return render_template('page.html')
 
-
-@app.route('/pred')
+@app.route('/pred', methods=['GET', 'POST'])
 def predict():
-    movie_detail = request.args.get('data')
+    data = request.args.get('data')
 
+    if request.method == 'POST' and 'movie_search' in request.form:
+        keyword = request.form.get("movie_search")
+        movie= requestIBMD(keyword)
+        print(movie)
     return render_template('prediction.html', data= data)
 
 
@@ -103,48 +107,55 @@ def send_message(message):
 @app.route('/p', methods=['GET', 'POST'])
 def presonality():
     if request.method == 'POST':
+        sig = 0
         #get the data from personality questionaire
-        i = 1
-        personality_detail = []
-        while i <=50:
-            q_result = request.form.get('q'+str(i))
-            if q_result == "a":
-                personality_detail.append(1)
-            elif q_result == "b":
-                personality_detail.append(2)
-            elif q_result == "c":
-                personality_detail.append(3)
-            elif q_result == "d":
-                personality_detail.append(4)
-            elif q_result == "e":
-                personality_detail.append(5)
-            else:
-                personality_detail.append(0)
-            i= i+1
-        #calculate the big fivepersonality
-        print()
-        j = 0
-        pe = 20
-        pa = 14
-        pc = 14
-        pn = 38
-        po = 8
-        while j < 10:
-            pe = pe + ((-1)**(j))* personality_detail[5*j]
-            pa = pa + ((-1)**(j+1))*personality_detail[1+5*j]
-            pc = pc + ((-1)**(j))* personality_detail[2+5*j]
-            pn = pn + ((-1)**(j+1))* personality_detail[3+5*j]
-            po = po + ((-1)**(j))* personality_detail[4+5*j]
-            j = j +1
-        big_five = [po/40, pc/40, pe/40, pa/40, pn/40]
-        print("big five are: ", str(big_five))
-        movie_detail = get_recommend(big_five, 20)
-        #handle movies data and store them into differrent categories
-        i = 0
         movies = []
-        while i < 20:
-            movies.append(Movie(movie_detail[i][1]))
-            i = i +1
+        if 'movie_search' in request.form:
+            sig = 1
+            print("Yes get movie_search")
+            keyword = request.form.get("movie_search")
+            movies= requestIBMD(keyword)
+            print(movies)
+        else:
+            i = 1
+            personality_detail = []
+            while i <=50:
+                q_result = request.form.get('q'+str(i))
+                if q_result == "a":
+                    personality_detail.append(1)
+                elif q_result == "b":
+                    personality_detail.append(2)
+                elif q_result == "c":
+                    personality_detail.append(3)
+                elif q_result == "d":
+                    personality_detail.append(4)
+                elif q_result == "e":
+                    personality_detail.append(5)
+                else:
+                    personality_detail.append(0)
+                i= i+1
+            #calculate the big fivepersonality
+            j = 0
+            pe = 20
+            pa = 14
+            pc = 14
+            pn = 38
+            po = 8
+            while j < 10:
+                pe = pe + ((-1)**(j))* personality_detail[5*j]
+                pa = pa + ((-1)**(j+1))*personality_detail[1+5*j]
+                pc = pc + ((-1)**(j))* personality_detail[2+5*j]
+                pn = pn + ((-1)**(j+1))* personality_detail[3+5*j]
+                po = po + ((-1)**(j))* personality_detail[4+5*j]
+                j = j +1
+            big_five = [po/40, pc/40, pe/40, pa/40, pn/40]
+            print("big five are: ", str(big_five))
+            movie_detail = get_recommend(big_five, 20)
+            #handle movies data and store them into differrent categories
+            i = 0
+            while i < 20:
+                movies.append.append(Movie(movie_detail[i][1]))
+                i = i +1
         j = 0
         imdbs = []
         images = []
@@ -157,24 +168,36 @@ def presonality():
         actors = []
         creators = []
         directors = []
-        while j < 20:
-            imdbs.append( "https://usa.newonnetflix.info/info/"+str(movie_detail[j][0]))
-            images.append(movies[j].image())
-            titles.append(unescape(movies[j].title()))
-            synopsises.append(unescape(movies[j].synopsis()))
-            releasedes.append(movies[j].released())
-            runtimes.append(movies[j].runtime())
-            genres.append(unescape(movies[j].genre()))
-            languages.append(movies[j].language())
-            actors.append(', '.join(map(str, unescape(movies[j].actor()))))
-            creators.append(', '.join(map(str, unescape(movies[j].creator()))))
-            directors.append(', '.join(map(str, unescape(movies[j].director()))))
+        while j < 5:
+            if sig==1:
+                imdbs.append(movies[j][0])
+                images.append(movies[j][1])
+                titles.append(movies[j][2])
+                synopsises.append(movies[j][3])
+                releasedes.append(movies[j][4])
+                runtimes.append(movies[j][5])
+                genres.append(movies[j][6])
+                languages.append(movies[j][7])
+                actors.append(movies[j][8])
+                creators.append(movies[j][9])
+                directors.append(movies[j][10])
+            else:
+                imdbs.append( "https://usa.newonnetflix.info/info/"+str(movie_detail[j][0]))
+                images.append(movies[j].image())
+                titles.append(unescape(movies[j].title()))
+                synopsises.append(unescape(movies[j].synopsis()))
+                releasedes.append(movies[j].released())
+                runtimes.append(movies[j].runtime())
+                genres.append(unescape(movies[j].genre()))
+                languages.append(movies[j].language())
+                actors.append(', '.join(map(str, unescape(movies[j].actor()))))
+                creators.append(', '.join(map(str, unescape(movies[j].creator()))))
+                directors.append(', '.join(map(str, unescape(movies[j].director()))))
             j = j+1
         print(titles)
         data = [imdbs, images,titles, synopsises,releasedes, runtimes, genres, languages, actors, creators, directors ]
         return render_template('prediction.html', data=data)
     return render_template('personality.html')
-
 
 
 #def questionnaire():
